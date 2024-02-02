1df825c82458d7667789495f7a88b14f3ff5fd4a
Javier Melendez <javi_lavis@hotmail.com>
2024-02-02 02:45:42 -0600

    Se agregaron un mierdo de cosas

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,25 @@
+function formatearPesos(valor){
+    const floatValue = parseFloat(valor);
+    if (!isNaN(floatValue)) {
+        const formattedValue = floatValue.toLocaleString('es-MX', {
+            style: 'currency',
+            currency: 'MXN',
+            maximumFractionDigits: 2
+        });
+        return formattedValue;
+    }
+}
+
 function calcularValor(valor){
     try{
-        let valorInmueble = document.getElementById("valorInmueble").value;
+        // let valorInmueble = document.getElementById("valorInmueble").value;
         let pagoMensual = document.getElementById("pagoMensual");
-        pago = valor / 100
-        pagoMensual.innerHTML = "$"+pago
+        let enganche = document.getElementById("enganche");
+        let engancheValue = formatearPesos(valor * .10);
+        let value = formatearPesos(valor/72) ;
+        pagoMensual.innerHTML = value ;
+        enganche.innerHTML = engancheValue
+        
     }catch(e){console.error("Ocurrió un error al simular el crédito")}
 }
 
